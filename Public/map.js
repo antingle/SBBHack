3d2272f1e9a32e993fb680dce248f027f71bc3c6
Anthony Ingle <ingleanthony@icloud.com>
2021-03-20 19:21:30 -0400

    cards do cool stuff

--- Public/map.js
+++ Public/map.js
@@ -1,11 +1,11 @@
-const apikey = 'AIzaSyDzKz9_k_DVMDKCVayOWK2sGuX6QDJ9gOo';
-const zipkey = 'f51cb250-8940-11eb-a563-317fa7c2b6b2';
+const apikey = "AIzaSyDzKz9_k_DVMDKCVayOWK2sGuX6QDJ9gOo";
+const zipkey = "f51cb250-8940-11eb-a563-317fa7c2b6b2";
 var meters = 20000;
 
 var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  mode: 'cors'
+  method: "GET",
+  redirect: "follow",
+  mode: "cors",
 };
 
 // Note: This example requires that you consent to location sharing when
@@ -22,58 +22,68 @@ function initMap() {
   });
 
   infoWindow = new google.maps.InfoWindow();
-  const nearButton = document.querySelector('.location-btn');
-  const radiusButtons = document.querySelectorAll('.dropdown-menu li a');
+  const nearButton = document.querySelector(".location-btn");
+  const radiusButtons = document.querySelectorAll(".dropdown-menu li a");
 
   for (let i = 0; i < radiusButtons.length; i++) {
     radiusButtons[i].addEventListener("click", () => {
       let km = radiusButtons[i].textContent.match(/\d+/)[0];
       meters = km * 1000;
-      document.querySelector('.radius-btn').textContent = `Radius (${km} km)`
+      document.querySelector(".radius-btn").textContent = `Radius (${km} km)`;
     });
   }
-  
+
   //search stations by postal code
-  let form = document.body.querySelector('.searchBar');
-  form.addEventListener('submit', () => {
-    let postal = form.elements['search'].value;
+  let form = document.body.querySelector(".searchBar");
+  form.addEventListener("submit", () => {
+    let postal = form.elements["search"].value;
     deleteMarkers();
 
     let fourDigits = /^\d{4}$/;
     if (fourDigits.test(postal) == false) {
-      if (document.getElementById('errorDiv') == null) {
-        let errorDiv = document.createElement('div');
-        errorDiv.id = ('errorDiv');
+      if (document.getElementById("errorDiv") == null) {
+        let errorDiv = document.createElement("div");
+        errorDiv.id = "errorDiv";
         errorDiv.innerHTML = "Incorrect Postal Code";
         document.body.insertBefore(errorDiv, form.parentNode);
       }
       return;
     } else {
-      let errorDiv = document.getElementById('errorDiv');
+      let errorDiv = document.getElementById("errorDiv");
       if (errorDiv) {
-      errorDiv.remove();
+        errorDiv.remove();
       }
     }
-    mapDiv = document.querySelector('#map');
-    mapDiv.scrollIntoView( { behavior: "smooth" } );
+    mapDiv = document.querySelector("#map");
+    mapDiv.scrollIntoView({ behavior: "smooth" });
     let pos;
-    fetch(`https://app.zipcodebase.com/api/v1/search?apikey=f51cb250-8940-11eb-a563-317fa7c2b6b2&codes=${postal}&country=CH`, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      let temp = result.results[postal];
-      let latitude = parseFloat(temp[0].latitude);
-      let longitude = parseFloat(temp[0].longitude);
-      pos = { lat: latitude, lng: longitude };
-      map.setCenter(pos);
-      map.setZoom(14 - meters/20000);
-      findStationsNear(pos);
-    });
+    fetch(
+      `https://app.zipcodebase.com/api/v1/search?apikey=f51cb250-8940-11eb-a563-317fa7c2b6b2&codes=${postal}&country=CH`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        let temp = result.results[postal];
+        let latitude = parseFloat(temp[0].latitude);
+        let longitude = parseFloat(temp[0].longitude);
+        pos = { lat: latitude, lng: longitude };
+
+        let marker = new google.maps.Marker({
+          position: pos,
+          map: map,
+          title: "Your position",
+        });
+        markers.push(marker);
+        map.setCenter(pos);
+        map.setZoom(14 - meters / 20000);
+        findStationsNear(pos);
+      });
   });
 
   //search stations by current location
   nearButton.addEventListener("click", () => {
-    mapDiv = document.querySelector('#map');
-    mapDiv.scrollIntoView( { behavior: 'smooth' } )
+    mapDiv = document.querySelector("#map");
+    mapDiv.scrollIntoView({ behavior: "smooth" });
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -87,11 +97,11 @@ function initMap() {
           let marker = new google.maps.Marker({
             position: pos,
             map: map,
-            title: 'Your position'
-        });
-          markers.push(marker); 
+            title: "Your position",
+          });
+          markers.push(marker);
           map.setCenter(pos);
-          map.setZoom(13);
+          map.setZoom(14 - meters / 20000);
           findStationsNear(pos);
         },
         () => {
@@ -131,64 +141,90 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 function findStationsNear(pos) {
   let lat = pos.lat;
   let lng = pos.lng;
-  fetch(`https://data.sbb.ch/api/records/1.0/search/?dataset=mobilitat&q=&rows=1000&facet=stationsbezeichnung&geofilter.distance=${lat}%2C${lng}%2C${meters}`, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-
+  const origin = [pos.lat, pos.lng];
+  fetch(
+    `https://data.sbb.ch/api/records/1.0/search/?dataset=mobilitat&q=&rows=1000&facet=stationsbezeichnung&geofilter.distance=${lat}%2C${lng}%2C${meters}`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
       //loops through results and creates a marker for each entry
       for (let i = 0; i < result.records.length; i++) {
         const coords = result.records[i].geometry.coordinates;
         const latLng = new google.maps.LatLng(coords[1], coords[0]);
-        let origin = [pos.lat, pos.lng];
-        let destination = coords[1] + ',' + coords[0];
+        const destination = coords[1] + "," + coords[0];
         var infowindow = new google.maps.InfoWindow({
-          content:  `
+          content: `
           <h2>${result.records[i].fields.abkuerzung} - ${result.records[i].fields.bezeichnung_offiziell}</h2>
-          `
-        });;
+          `,
+        });
 
-        //set custom marker 
+        //set custom marker
         let marker = new google.maps.Marker({
           position: latLng,
           map: map,
           animation: google.maps.Animation.DROP,
           icon: {
-            url: 'parking.png',
-            scaledSize: new google.maps.Size(32,32)
+            url: "parking.png",
+            scaledSize: new google.maps.Size(32, 32),
           },
-          title: result.records[i].fields.abkuerzung
+          title: result.records[i].fields.abkuerzung,
         });
         markers.push(marker);
 
         //adds address and eta to marker window
         marker.addListener("click", () => {
-
-          fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=AIzaSyDzKz9_k_DVMDKCVayOWK2sGuX6QDJ9gOo`, requestOptions)
-          .then(response => response.json())
-          .then(result2 => {
-            infowindow = new google.maps.InfoWindow({
-              content:  `
+          fetch(
+            `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=AIzaSyDzKz9_k_DVMDKCVayOWK2sGuX6QDJ9gOo`,
+            requestOptions
+          )
+            .then((response) => response.json())
+            .then((result2) => {
+              infowindow = new google.maps.InfoWindow({
+                content: `
               <h2>${result.records[i].fields.abkuerzung} - ${result.records[i].fields.bezeichnung_offiziell}</h2>
               <p><a href = "https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}">${result2.destination_addresses[0]}</a></p>
               <p>Entfernung: <b>${result2.rows[0].elements[0].distance.text}</b></p>
               <p>Geschätzte Zeit: <b>${result2.rows[0].elements[0].duration.text}</b></p>
-              `
-            });
+              `,
+              });
               infowindow.open(map, marker);
-          })
-          .catch(error => console.log('error', error));
-
+            })
+            .catch((error) => console.log("error", error));
         });
-        google.maps.event.addListener(map, "click", function(event) {
+        google.maps.event.addListener(map, "click", function (event) {
           infowindow.close();
-      });
+        });
       }
+      
       //set card texts
-      let card = document.body.querySelectorAll('.parking-card');
+      let card = document.body.querySelectorAll(".parking-card");
       for (let i = 0, j = 0; i < card.length; i++, j++) {
-        while (result.records[j].fields.bezeichnung_offiziell == result.records[j+1].fields.bezeichnung_offiziell) j++;
-        card[i].childNodes[1].textContent = `${result.records[j].fields.abkuerzung} - ${result.records[j].fields.bezeichnung_offiziell}`;
+        while (result.records[j].fields.bezeichnung_offiziell == result.records[j + 1].fields.bezeichnung_offiziell) j++;
+
+        const coords = result.records[j].geometry.coordinates;
+        const destination = coords[1] + "," + coords[0];
+        fetch(
+          `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=AIzaSyDzKz9_k_DVMDKCVayOWK2sGuX6QDJ9gOo`,
+          requestOptions
+        )
+          .then((response) => response.json())
+          .then((result2) => {
+
+            let p = document.createElement("p");
+            let p2 = document.createElement("p");
+            card[i].childNodes[1].textContent = `${result.records[j].fields.abkuerzung} - ${result.records[j].fields.bezeichnung_offiziell}`;
+            p.innerHTML = `Entfernung: <b>${result2.rows[0].elements[0].distance.text}</b>`;
+            card[i].insertBefore(p, card[i].childNodes[2]);
+            p2.innerHTML = `<p>Geschätzte Zeit: <b>${result2.rows[0].elements[0].duration.text}</b></p>`;
+            card[i].insertBefore(p2, card[i].childNodes[3]);
+            let p3 = document.createElement("p");
+            p3.innerHTML = `<p><a href = "https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}">${result2.destination_addresses[0]}</a></p>`;
+            card[i].insertBefore(p3, card[i].childNodes[5]);
+            card[i].hidden
+          });
       }
+      document.querySelector('.parking-card-deck').hidden = false;
     })
-    .catch(error => console.log('error', error));
+    .catch((error) => console.log("error", error));
 }
