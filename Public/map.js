d40e19aad8bbd444fc216d389f460a947be41104
Anthony Ingle <ingleanthony@icloud.com>
2021-03-20 16:23:52 -0400

    moved files

--- Public/map.js
+++ Public/map.js
@@ -4,7 +4,8 @@ var meters = 20000;
 
 var requestOptions = {
   method: 'GET',
-  redirect: 'follow'
+  redirect: 'follow',
+  mode: 'cors'
 };
 
 // Note: This example requires that you consent to location sharing when
@@ -16,7 +17,7 @@ let map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 47.5474120551, lng: 7.58956279016 },
-    zoom: 12,
+    zoom: 10,
   });
 
   infoWindow = new google.maps.InfoWindow();
@@ -53,6 +54,7 @@ function initMap() {
       let longitude = parseFloat(temp[0].longitude);
       pos = { lat: latitude, lng: longitude };
       map.setCenter(pos);
+      map.setZoom(12);
       findStationsNear(pos);
     });
   });
@@ -78,6 +80,7 @@ function initMap() {
         });
           // infoWindow.open(map);
           map.setCenter(pos);
+          map.setZoom(12);
           findStationsNear(pos);
         },
         () => {
@@ -120,17 +123,20 @@ function findStationsNear(pos) {
   fetch(`https://data.sbb.ch/api/records/1.0/search/?dataset=mobilitat&q=&rows=1000&facet=stationsbezeichnung&geofilter.distance=${lat}%2C${lng}%2C${meters}`, requestOptions)
     .then(response => response.json())
     .then(result => {
-      console.log(result.records);
+
+      //loops through results and creates a marker for each entry
       for (let i = 0; i < result.records.length; i++) {
         const coords = result.records[i].geometry.coordinates;
         const latLng = new google.maps.LatLng(coords[1], coords[0]);
-        const infowindow = new google.maps.InfoWindow({
+        let origin = [pos.lat, pos.lng];
+        let destination = coords[1] + ',' + coords[0];
+        var infowindow = new google.maps.InfoWindow({
           content:  `
-          
           <h2>${result.records[i].fields.abkuerzung} &nbsp; ${result.records[i].fields.bezeichnung_offiziell}</h2>
-          <p>${result.records[i].fields.veloparking_status_d}</p>
           `
-        });
+        });;
+
+        //set custom marker 
         let marker = new google.maps.Marker({
           position: latLng,
           map: map,
@@ -141,26 +147,36 @@ function findStationsNear(pos) {
           },
           title: result.records[i].fields.abkuerzung
         });
+
+        //adds address and eta to marker window
         marker.addListener("click", () => {
-          infowindow.open(map, marker);
+
+          fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=AIzaSyDzKz9_k_DVMDKCVayOWK2sGuX6QDJ9gOo`, requestOptions)
+          .then(response => response.json())
+          .then(result2 => {
+            console.log(result2.rows[0].elements[0]);
+            infowindow = new google.maps.InfoWindow({
+              content:  `
+              <h2>${result.records[i].fields.abkuerzung} &nbsp; ${result.records[i].fields.bezeichnung_offiziell}</h2>
+              <p>${result2.destination_addresses[0]}</p>
+              `
+            });
+              infowindow.open(map, marker);
+          })
+          .catch(error => console.log('error', error));
+
         });
         google.maps.event.addListener(map, "click", function(event) {
           infowindow.close();
       });
       }
-      let card = document.body.querySelectorAll('.card-deck .card .card-block h2');
+      //set card texts
+      let card = document.body.querySelectorAll('.parking-card-deck .parking-card h2');
       for (let i = 0, j = 0; i < card.length; i++, j++) {
         console.log(result.records[j].fields.bezeichnung_offiziell);
         while (result.records[j].fields.bezeichnung_offiziell == result.records[j+1].fields.bezeichnung_offiziell) j++;
-        card[i].innerHTML = `${result.records[j].fields.abkuerzung} &nbsp; ${result.records[i].fields.bezeichnung_offiziell}`;
+        card[i].innerHTML = `${result.records[j].fields.abkuerzung} &nbsp; ${result.records[j].fields.bezeichnung_offiziell}`;
       }
     })
     .catch(error => console.log('error', error));
 }
-// const pr = "pr.png";
-const iconBase =
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
-  const icons = {
-    parking: iconBase + "parking_lot_maps.png"
-  };
-  
