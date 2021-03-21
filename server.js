e62aa491d7cf9377237d4afd83569bc19c8a3d0d
Anthony Ingle <ingleanthony@icloud.com>
2021-03-21 03:29:18 -0400

    cors cringe

--- server.js
+++ server.js
@@ -6,13 +6,29 @@ const app = express();
 
 //some cringe cors error I have to fix
 app.use(cors());
-
 app.listen(3000);
+
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
+   app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
+
 
 // declares the statics folder
 app.use(express.static('public'));
 
-app.get('/', (req, res) => { app.sendFile('/index.html') });
+app.get('/', (req, res, next) => { app.sendFile('/index.html') });
 
 app.post('/model', (req, res) => {
     let hours = req.query.hours;
@@ -20,3 +36,17 @@ app.post('/model', (req, res) => {
     console.log(hours, name);
     res.send(`${parseInt(hours)}`);
 });
+
+app.get('/google', (req, res) => {
+    let url = req.query.url;
+    request(
+      { url: url },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          return res.status(500).json({ type: 'error', message: err.message });
+        }
+  
+        res.json(JSON.parse(body));
+      }
+    )
+  });
